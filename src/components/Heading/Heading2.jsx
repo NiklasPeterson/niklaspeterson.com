2896a2121a2461af646e73d87f4b89731ac15508
Niklas Peterson <design.npeterson@icloud.com>
2020-12-11 14:12:07 +0100

    Adding react project files

---
+++ src/components/Heading/Heading2.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components/macro";
+
+const Heading2 = (props) => <ScH2>{props.text}</ScH2>;
+
+export default Heading2;
+
+const ScH2 = styled.h2`
+	margin-bottom: 24px;
+`;
