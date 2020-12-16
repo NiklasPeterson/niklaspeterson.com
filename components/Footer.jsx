a42caffbc8a386ebd3b99bc86e0a0a6c31ec19a3
Niklas Peterson <5664205+NiklasPeterson@users.noreply.github.com>
2020-12-16 13:04:59 +0100

    pushing changes

---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Footer = (props) => (
+	<ScFooter>{props.children}</ScFooter>
+);
+
+export default Footer;
+
+const ScFooter = styled.article`
+	margin-top: 40px;
+`;
