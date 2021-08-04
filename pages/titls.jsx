3b762d55f78f4718044a8689e80697ffccedaa50
Niklas Peterson <5664205+NiklasPeterson@users.noreply.github.com>
2021-08-04 15:57:11 +0200

    Creating a landing page for Titls iOS App

---
+++ pages/titls.jsx
@@ -0,0 +1,53 @@
+import Heading2 from '@components/Heading/Heading2'
+import Article from '@components/Article'
+import Copyright from '@components/Copyright'
+import Footer from '@components/Footer'
+import Layout from '@components/Layout'
+import Header from '@components/Header'
+import AppStoreButton from '@components/AppStoreButton'
+
+// import AppStoreWhite from '/images/App_store-white.svg';
+// import AppStoreBlack from '/images/App_store-black.svg';
+
+export default function Home(props) {
+  return (
+    <Layout
+      pageTitle={props.title}
+      pageDescription={props.description}
+      pageAuthor={props.author}
+      dribbbleLink={props.dribbble}
+      githubLink={props.github}
+      twitterLink={props.twitter}
+      instagramLink={props.instagram}
+      video={'/titls_app-preview.mp4'}
+    >
+      <Header heading="Titls" tagline="iOS widget" />
+      <Article>
+        <Heading2
+          text="Titls is an small iOS app that lets you create titles and then use widgets to organise your iOS homescreens."
+        />
+          <AppStoreButton url="https://apps.apple.com/us/app/titls/id1579078964"/>
+      </Article>
+
+      <Footer>
+        <Copyright text="&copy; 2021 Niklas Peterson" />
+      </Footer>
+    </Layout>
+  )
+}
+
+export async function getStaticProps() {
+  const config = await import(`../siteconfig.json`)
+
+  return {
+    props: {
+      title: config.default.title,
+      author: config.default.author,
+      description: config.default.description,
+      dribbble: config.default.dribbble,
+      github: config.default.github,
+      twitter: config.default.twitter,
+      instagram: config.default.instagram,
+    },
+  }
+}
