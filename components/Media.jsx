05a213bbe7592b5bf144eb7f6564e4c0620cad20
Niklas Peterson <5664205+NiklasPeterson@users.noreply.github.com>
2021-08-19 19:04:00 +0200

    Fixing media placement

--- components/Media.jsx
+++ components/Media.jsx
@@ -10,7 +10,7 @@ const Media = (props) => {
     <Image src={props.image} alt={props.altText} layout="fill" objectPosition="center center" objectFit="cover" placeholder="blur" priority={true}/>
   }
   {props.video &&
-    <video style={{height: '100%'}} playsInline muted autoPlay loop >
+    <video style={{height: '100%', width: '100%'}} playsInline muted autoPlay loop>
       <source src={props.video} type="video/mp4" />
       Your browser does not support the video tag.
     </video>
@@ -25,34 +25,6 @@ const ScMedia = styled.div`
   width: 50vw;
   height: 100vh;
   overflow: hidden;
-
-  @media (max-width: 812px) {
-    position: fixed;
-    z-index: -1;
-    width: 100vw;
-
-    &:after {
-      content: '';
-      position: fixed;
-      top: 0;
-      right: 0;
-      bottom: 0;
-      left: 0;
-      background-color: var(--color-background);
-      opacity: 0.88;
-    }
-  }
-
-  ${(props) =>
-    props.video &&
-    css`
-      text-align: center;
-
-      @media (max-width: 812px) {
-        width: 100vw;
-        text-align: center;
-      }
-    `};
 
   ${(props) =>
     props.image &&
@@ -77,7 +49,20 @@ const ScMedia = styled.div`
           opacity: 0.88;
         }
       }
-    `};
+    `
+  };
 
-  
+  ${(props) =>
+  props.video &&
+  css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    @media (max-width: 812px) {
+      width: 100vw;
+      height: auto;
+    }
+  `
+  };
 `
