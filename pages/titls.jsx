b04d461673e0aaa0c8edd2aded07253b0bb17229
Niklas Peterson <5664205+NiklasPeterson@users.noreply.github.com>
2023-01-31 09:02:03 +0100

    Update titls.jsx

--- pages/titls.jsx
+++ pages/titls.jsx
@@ -45,7 +45,7 @@ export default function Titls(props) {
       </Article>
 
       <Footer>
-        <Copyright text="&copy; 2021 Niklas Peterson." />
+        <Copyright text="&copy; 2023 Niklas Peterson." />
       </Footer>
     </Layout>
   )
