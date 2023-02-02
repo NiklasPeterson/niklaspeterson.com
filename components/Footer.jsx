550074de25303a3d0c3ddd00731842dcf41b1238
Niklas Peterson <5664205+NiklasPeterson@users.noreply.github.com>
2023-02-02 19:54:50 +0000

    Clean slate

--- components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-const Footer = (props) => <ScFooter>{props.children}</ScFooter>
-
-export default Footer
-
-const ScFooter = styled.footer`
-  margin-top: 40px;
-`
