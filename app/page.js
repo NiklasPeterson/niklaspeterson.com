06efc182239c7fc8269e3d8e18dbb69dd5ab067e
Niklas Peterson <5664205+NiklasPeterson@users.noreply.github.com>
2024-03-28 15:12:22 +0000

    Adjusting button styling

--- app/page.js
+++ app/page.js
@@ -26,7 +26,7 @@ export default function Home() {
           />
         </svg>
 
-        <a href='mailto:hi@niklaspeterson.com' className='text-base font-medium rounded-full px-5 py-3 flex gap-1 items-center text-zinc-50 bg-zinc-900 hover:bg-zinc-700 focus:outline-none focus:ring-4 focus:ring-zinc-300 dark:text-zinc-950 dark:bg-zinc-50 dark:hover:bg-zinc-200 dark:focus:ring-zinc-100'>
+        <a href='mailto:hi@niklaspeterson.com' className='px-5 py-3 text-base btn-primary'>
           <span className='mx-1'>Say Hello</span>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 0 1-.923 1.785A5.969 5.969 0 0 0 6 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337Z" />
@@ -126,16 +126,15 @@ export default function Home() {
               <br /><br />
               Outside work, you&apos;ll find me cherishing quality time with my family. In my free moments, I enjoy exploring design and coding. I&apos;ve created two iOS apps, <span className='font-semibold text-zinc-950 dark:text-zinc-50'>Hydrify</span> and <span className='font-semibold text-zinc-950 dark:text-zinc-50'>Titls</span>. These are personal projects that let me scratch that creative itch by seamlessly blending design, development, and tech.</p>
           </div>
-
           <div className="flex gap-2">
-            <a href="https://www.buymeacoffee.com/niklaspeterson" className="btn-primary">
+            <a href="https://www.buymeacoffee.com/niklaspeterson" className="h-10 text-sm btn-primary">
               <span className='mx-1'>Support my work</span>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
               </svg>
             </a>
 
-            <a href="https://read.cv/niklaspeterson" className="btn-secondary">
+            <a href="https://read.cv/niklaspeterson" className="h-10 text-sm btn-secondary">
               <span className='mx-1'>Resumé</span>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4">
                 <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
@@ -144,9 +143,9 @@ export default function Home() {
           </div>
         </div>
 
-        <div className='relative h-[320px] w-full sm:h-[400px] sm:w-[320px]'>
+        <div className='relative rounded-2xl md:rounded-3xl overflow-hidden h-[320px] w-full sm:h-[400px] sm:w-[320px]'>
           <Image
-            className='rounded-2xl md:rounded-3xl'
+            className='transition-transform duration-300 hover:scale-105'
             src="/niklas-peterson.jpg"
             alt="Picture of Niklas Peterson"
             fill={true}
