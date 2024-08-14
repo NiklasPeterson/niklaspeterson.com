038d7a55f8e1ea72d973bfabf22a657ad3bac796
Niklas Peterson <5664205+NiklasPeterson@users.noreply.github.com>
2024-08-14 23:42:55 +0200

    Updating resume link

--- app/page.js
+++ app/page.js
@@ -135,7 +135,7 @@ export default function Home() {
               </svg>
             </a>
 
-            <a href="https://read.cv/niklaspeterson" className="h-10 text-sm btn-secondary">
+            <a href="https://cv.niklaspeterson.com" className="h-10 text-sm btn-secondary">
               <span className='mx-1'>Resumé</span>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4">
                 <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
