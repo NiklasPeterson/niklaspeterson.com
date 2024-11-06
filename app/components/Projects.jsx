178fb4454d24a115a8b7abda6a505d4fb4c23d1b
NiklasPeterson <5664205+NiklasPeterson@users.noreply.github.com>
2024-11-06 16:51:35 +0100

    fixes UI bug for border on project modal on mobile

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -42,7 +42,7 @@ export default function Projects() {
 
       {selectedProject && (
         <div className="fixed bg-[rgba(0,0,0,0.25)] z-10 top-0 right-0 bottom-0 left-0 h-svh w-svw overflow-auto md:p-4">
-          <div className="min-h-full flex flex-col rounded-2xl py-6 md:py-10 gap-10 bg-white dark:bg-black opacity-0 border dark:md:border-zinc-800 fade-slide-in">
+          <div className="min-h-full flex flex-col rounded-2xl py-6 md:py-10 gap-10 bg-white dark:bg-black opacity-0 dark:md:border dark:md:border-zinc-800 fade-slide-in">
 
             <div className="flex flex-col gap-6 md:gap-10 px-4 md:px-10">
 
