92699be536099686835bafcdfa12c9e86367bbb5
Niklas Peterson <5664205+NiklasPeterson@users.noreply.github.com>
2025-03-05 17:32:35 +0100

    Update About.jsx

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -15,13 +15,13 @@ export default function About() {
         </div>
         <div className="flex gap-2">
           <a href="https://www.buymeacoffee.com/niklaspeterson" target="_blank" className="h-10 text-sm btn-primary">
-            <span className='mx-1'>Read my CV</span>
+            <span className='mx-1'>Support my work</span>
             {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4">
               <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
             </svg> */}
           </a>
           <a href="https://cv.niklaspeterson.com" target="_blank" className="h-10 text-sm btn-secondary">
-            <span className='mx-1'>Support my work</span>
+            <span className='mx-1'>Read my CV</span>
           </a>
         </div>
       </div>
