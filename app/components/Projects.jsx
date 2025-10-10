8097d40710607023c3e68fff93aebbe0ad6d46d4
Niklas Peterson <5664205+NiklasPeterson@users.noreply.github.com>
2025-10-10 13:31:31 +0200

    adds md:h-fit to project modal

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -44,7 +44,7 @@ export default function Projects() {
 
     {selectedProject && (
         <div className="fixed bg-white/25 dark:bg-black/25 backdrop-blur-lg z-10 top-0 right-0 bottom-0 left-0 h-screen w-screen overflow-auto md:p-10">
-          <div className="min-h-full flex flex-col md:rounded-2xl pt-6 md:pt-10 pb-1 md:pb-2 gap-10 md:border border-zinc-200 bg-white dark:bg-black dark:md:border-zinc-800 animate-fadeUp mx-auto max-w-[1440px]">
+          <div className="min-h-full md:h-fit flex flex-col md:rounded-2xl pt-6 md:pt-10 pb-1 md:pb-2 gap-10 md:border border-zinc-200 bg-white dark:bg-black dark:md:border-zinc-800 animate-fadeUp mx-auto max-w-[1440px]">
 
             <div className="flex flex-col gap-6 md:gap-8 px-4 md:px-10">
 
