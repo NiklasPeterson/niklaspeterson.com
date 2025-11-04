1f150bf9316a3f1a7b747a95d55d126bd31a3bc1
Niklas Peterson <5664205+NiklasPeterson@users.noreply.github.com>
2025-11-04 19:13:18 +0100

    feat: add redirects for /titls and /hydrify to external applications

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+      return [
+        {
+          source: '/titls',
+          destination: 'https://titls.app',
+          permanent: true, // 308 redirect
+        },
+        {
+          source: '/hydrify',
+          destination: 'https://hydrify.app',
+          permanent: true, // 308 redirect
+        },
+      ];
+    },
+  };
+  
 
 export default nextConfig;
+
