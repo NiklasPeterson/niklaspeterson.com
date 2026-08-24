42429d571e8aa9baa3644de802d55228f3b37620
Niklas Peterson <5664205+NiklasPeterson@users.noreply.github.com>
2026-08-24 20:36:40 +0200

    refactor: streamline homepage layout and styling

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -4,61 +4,54 @@ import HoverThumbnail from "./HoverThumbnail";
 export default function Header() {
   return (
     <FadeIn
-        position="down"
-        desktopOnly
-        className="relative z-1 flex h-content flex-col justify-center gap-4 overflow-visible py-20 px-4 md:px-20 md:py-32"
-      >
-        {/* <div className="absolute top-0 right-0 left-0 -z-10 flex max-w-full justify-center overflow-hidden ">
-          <img src="/animated-header.svg" alt="Animated Header" />
-        </div> */}
-        {/* <div className="flex rounded-2xl gap-1 items-center bg-zinc-200 dark:bg-zinc-800 w-fit py-1 px-3">
-          <div className="rounded-2xl w-3 h-3 bg-red-500"></div>
-          <span className="mx-1 text-zinc-950 dark:text-zinc-50">Not open for work</span>
-        </div> */}
-        <h1 className="max-w-4xl text-4xl leading-tight font-semibold text-zinc-950 md:text-[64px] dark:text-zinc-50">
-          Bringing digital products to life with pixels and code.
-        </h1>
-        <p className="max-w-4xl text-xl text-pretty md:text-2xl">
-          Niklas Peterson, designer and creator from Sweden, currently shaping
-          experiences as a{" "}
-          <span className="font-semibold text-zinc-950 dark:text-zinc-50">
-            Staff Product Designer
-          </span>{" "}
-          at{" "}
-          <span className="inline-block font-semibold text-zinc-950 dark:text-zinc-50">
-            LottieFiles
-          </span>
-          .
-        </p>
-        <p className="text-lg md:text-xl">
-          In my free time I&apos;m building{" "}
-          <a
-            href="https://apps.apple.com/app/hydrify/id6450311759"
-            target="_blank"
-            className="inline-block font-semibold text-zinc-950 dark:text-zinc-50"
-          >
-            <HoverThumbnail
-              text="Hydrify"
-              images={["/hydrify-screen-1.png", "/hydrify-screen-2.png"]}
-            />
-          </a>{" "}
-          and{" "}
-          <a
-            href="https://apps.apple.com/app/titls/id1579078964"
-            target="_blank"
-            className="inline-block font-semibold text-zinc-950 dark:text-zinc-50"
-          >
-            <HoverThumbnail
-              text="Titls"
-              images={[
-                "/titls-screen-2.png",
-                "/titls-screen-3.png",
-                "/titls-screen-1.png",
-              ]}
-            />
-          </a>
-          .
-        </p>
-      </FadeIn>
+      position="down"
+      desktopOnly
+      className="relative z-1 flex h-content flex-col justify-center gap-4 overflow-visible px-4 pt-20 pb-24 md:px-20 md:py-32"
+    >
+      <h1 className="max-w-4xl text-4xl leading-tight text-pretty font-semibold text-primary md:text-6xl">
+        Bringing digital products to life with pixels and code.
+      </h1>
+      <p className="max-w-3xl text-xl text-pretty md:text-2xl">
+        Niklas Peterson, designer and creator from Sweden, currently shaping
+        experiences as a{" "}
+        <span className="font-semibold text-primary">
+          Staff Product Designer
+        </span>{" "}
+        at{" "}
+        <span className="inline-block font-semibold text-primary">
+          LottieFiles
+        </span>
+        .
+      </p>
+      <p className="text-lg md:text-xl">
+        In my free time I&apos;m building{" "}
+        <a
+          href="https://apps.apple.com/app/hydrify/id6450311759"
+          target="_blank"
+          className="inline-block font-semibold text-primary"
+        >
+          <HoverThumbnail
+            text="Hydrify"
+            images={["/hydrify-screen-1.png", "/hydrify-screen-2.png"]}
+          />
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://apps.apple.com/app/titls/id1579078964"
+          target="_blank"
+          className="inline-block font-semibold text-primary"
+        >
+          <HoverThumbnail
+            text="Titls"
+            images={[
+              "/titls-screen-2.png",
+              "/titls-screen-3.png",
+              "/titls-screen-1.png",
+            ]}
+          />
+        </a>
+        .
+      </p>
+    </FadeIn>
   );
 }
