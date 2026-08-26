29096287452bfbd7f30012f8efaeb4414d8ea9b8
Niklas Peterson <5664205+NiklasPeterson@users.noreply.github.com>
2026-08-26 16:30:35 +0200

    fix: adjust blur effect in LottieAnimation component for improved visual clarity

--- app/components/LottieAnimation.jsx
+++ app/components/LottieAnimation.jsx
@@ -9,7 +9,7 @@ export function LottieAnimation() {
       autoplay
       loop
       aria-hidden="true"
-      className="w-full h-fit blur-[120px]"
+      className="w-full h-fit blur-3xl md:blur-[120px]"
     />
   );
 }
