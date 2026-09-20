29ff9cf6e5d27e1dff3e4934a726ed50b7354a1e
Niklas Peterson <5664205+NiklasPeterson@users.noreply.github.com>
2026-09-20 23:45:58 +0200

    Fix import statement for previewImage5

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -2,7 +2,7 @@ import previewImage1 from "../../public/media/hydrify-screen-1-4770c7a49ef8.webp
 import previewImage2 from "../../public/media/hydrify-screen-2-add4de8285bb.webp";
 import previewImage3 from "../../public/media/titls-screen-2-fb3c1eb1b9b1.png";
 import previewImage4 from "../../public/media/titls-screen-3-f7896240f34f.png";
-import previewImage5 from "../../public/media/titls-screen-1-4b046518ef87.png"";
+import previewImage5 from "../../public/media/titls-screen-1-4b046518ef87.png";
 import FadeIn from "./FadeIn";
 import HoverThumbnail from "./HoverThumbnail";
 
